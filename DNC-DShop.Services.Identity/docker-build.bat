1b5bf337f6d51ed9e6e3e3e61b6295c6edd60ce0
micky-g <michael@mwgtechnologies.co.uk>
2020-02-12 15:43:07 +0000

    removing redundant k8s dockerfile

--- DNC-DShop.Services.Identity/docker-build.bat
+++ DNC-DShop.Services.Identity/docker-build.bat
@@ -8,7 +8,7 @@ REM Login to docker
 docker login
 
 REM Build the docker image 
-docker build -f Dockerfile.k8s -t michaelgroenewald/k8s.dshop.services.identity .
+docker build -f Dockerfile -t michaelgroenewald/k8s.dshop.services.identity .
 
 REM Push docker image to the Docker Hub
 docker push michaelgroenewald/k8s.dshop.services.identity:latest
--- DNC-DShop.Services.Signalr/docker-build.bat
+++ DNC-DShop.Services.Signalr/docker-build.bat
@@ -8,7 +8,7 @@ REM Login to docker
 docker login
 
 REM Build the docker image 
-docker build -f Dockerfile.k8s -t michaelgroenewald/k8s.dshop.services.signalr .
+docker build -f Dockerfile -t michaelgroenewald/k8s.dshop.services.signalr .
 
 REM Push docker image to the Docker Hub
 docker push michaelgroenewald/k8s.dshop.services.signalr:latest
